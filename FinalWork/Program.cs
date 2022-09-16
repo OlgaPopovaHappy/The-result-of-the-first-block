d94c6e97af23eed8d23ce850542527ce7d6b9990
OlgaPopova <109219990+OlgaPopovaHappy@users.noreply.github.com>
2022-09-17 01:06:05 +0300

    Два метода объеденила в один

--- FinalWork/Program.cs
+++ FinalWork/Program.cs
@@ -15,25 +15,19 @@ WriteLine("Задайте массив строк через пробел: ");
 string[] array = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
 string[] newArray = new string[array.Length];
 SelectFromArray(array, newArray);
-PrintArray(newArray);
 
 void SelectFromArray(string[] array1, string[] array2)
 {
     int count = 0;
+    Write($"[ ");
     for (int i = 0; i < array1.Length; i++)
     {
         if (array1[i].Length <= 3)
         {
             array2[count] = array1[i];
+            Write($"{array1[i]} ");
             count++;
         }
     }
-}
-
-void PrintArray(string[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        Write($"{array[i]} ");
-    }
+    Write($"]");
 }
