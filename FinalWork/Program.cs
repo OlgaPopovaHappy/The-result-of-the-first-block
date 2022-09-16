031c3316d3b1c416e43ce100ea3d1eb473071f31
OlgaPopova <109219990+OlgaPopovaHappy@users.noreply.github.com>
2022-09-17 00:13:11 +0300

    Removed brackets

--- FinalWork/Program.cs
+++ FinalWork/Program.cs
@@ -15,29 +15,25 @@ WriteLine("Задайте массив строк через пробел: ");
 string[] array = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
 string[] newArray = new string[array.Length];
 SelectFromArray(array, newArray);
-// PrintArray(newArray);
+PrintArray(newArray);
 
 void SelectFromArray(string[] array1, string[] array2)
 {
     int count = 0;
-    Write($"[");
     for (int i = 0; i < array1.Length; i++)
     {
         if (array1[i].Length <= 3)
         {
             array2[count] = array1[i];
-            Write($"{array1[i]}");
             count++;
         }
     }
-    Write($"]");
 }
 
-// void PrintArray(string[] array)
-// {
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         Write($"{array[i]} ");
-//     }
-//     WriteLine();
-// }
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Write($"{array[i]} ");
+    }
+}
